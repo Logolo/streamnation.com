e80dbe933c2e91679dbc21300a907c680a974467
Justin Falcone <stanulam59@gmail.com>
2013-02-24 13:06:57 -0500

    sketch up features page

--- features.php
+++ features.php
@@ -1,5 +1,153 @@
 <?php require 'header.php' ?>
 
-<h1>TBD: Features</h1>
+<section class="features-header">
+  <div class="container">
+    <h2 class="subheader">Take the</h2>
+    <h1>Stream Nation Tour</h1>
+  </div>
+</section>
+<section class="features-section-alt">
+  <header class="section-header">
+    <h2 class="subheader">Download the</h2>
+    <h1>Stream Nation Folder</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section">
+  <header class="section-header">
+    <h2 class="subheader">Start uploading</h2>
+    <h1>Your Videos</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section-alt">
+  <header class="section-header">
+    <h2 class="subheader">You can watch</h2>
+    <h1>On Any Device</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section">
+  <header class="section-header">
+    <h2 class="subheader">Now your videos</h2>
+    <h1>Live In The Cloud</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section-alt">
+  <header class="section-header">
+    <h2 class="subheader">Feel secure knowing</h2>
+    <h1>Your Videos Are Safe</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section">
+  <header class="section-header">
+    <h2 class="subheader">Now you can</h2>
+    <h1>Watch Your Videos</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section-alt">
+  <header class="section-header">
+    <h2 class="subheader">Find your friends</h2>
+    <h1>And Get Social</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section">
+  <div class="request-invite">
+    <h1>Request an invite to get started</h1>
+  </div>
+  <div class="sign-up-container">
+    <a href="#" class="sign-up">
+      <img src="img/png/stamps/Request_blue.png" alt="Sign up here">
+    </a>
+  </div>
+</section>
 
 <?php require 'footer.php' ?>
