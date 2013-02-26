81ac20c45ad628f6781a74f18d93e073ec282a8c
Justin Falcone <stanulam59@gmail.com>
2013-02-25 21:54:08 -0500

    features page

--- features.php
+++ features.php
@@ -2,21 +2,20 @@
 
 <section class="features-header">
   <div class="container">
-    <h2 class="subheader">Take the</h2>
     <h1>Stream Nation Tour</h1>
   </div>
 </section>
 <section class="features-section-alt">
   <header class="section-header">
-    <h2 class="subheader">Download the</h2>
-    <h1>Stream Nation Folder</h1>
+    <h2 class="subheader">Start uploading</h2>
+    <h1>Your Videos &amp; Movies</h1>
   </header>
   <div class="row">
     <div class="features-block">
       <img src="">
     </div>
     <div class="features-block">
-      <p>
+      <p class="summary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
       <p>
@@ -26,25 +25,6 @@
   </div>
 </section>
 <section class="features-section">
-  <header class="section-header">
-    <h2 class="subheader">Start uploading</h2>
-    <h1>Your Videos</h1>
-  </header>
-  <div class="row">
-    <div class="features-block">
-      <img src="">
-    </div>
-    <div class="features-block">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-      <p>
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
-      </p>
-    </div>
-  </div>
-</section>
-<section class="features-section-alt">
   <header class="section-header">
     <h2 class="subheader">You can watch</h2>
     <h1>On Any Device</h1>
@@ -54,7 +34,26 @@
       <img src="">
     </div>
     <div class="features-block">
+      <p class="summary">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
       <p>
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+      </p>
+    </div>
+  </div>
+</section>
+<section class="features-section-alt">
+  <header class="section-header">
+    <h2 class="subheader">Invite your trusted friends &amp;</h2>
+    <h1>Open Your Collections</h1>
+  </header>
+  <div class="row">
+    <div class="features-block">
+      <img src="">
+    </div>
+    <div class="features-block">
+      <p class="summary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
       <p>
@@ -65,15 +64,15 @@
 </section>
 <section class="features-section">
   <header class="section-header">
-    <h2 class="subheader">Now your videos</h2>
-    <h1>Live In The Cloud</h1>
+    <h2 class="subheader">Browse your friends' collections &amp;</h2>
+    <h1>Borrow Their Movies</h1>
   </header>
   <div class="row">
     <div class="features-block">
       <img src="">
     </div>
     <div class="features-block">
-      <p>
+      <p class="summary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
       <p>
@@ -92,7 +91,7 @@
       <img src="">
     </div>
     <div class="features-block">
-      <p>
+      <p class="summary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
       <p>
@@ -103,15 +102,15 @@
 </section>
 <section class="features-section">
   <header class="section-header">
-    <h2 class="subheader">Now you can</h2>
-    <h1>Watch Your Videos</h1>
+    <h2 class="subheader">Your account and videos</h2>
+    <h1>Are Private</h1>
   </header>
   <div class="row">
     <div class="features-block">
       <img src="">
     </div>
     <div class="features-block">
-      <p>
+      <p class="summary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
       <p>
@@ -122,15 +121,15 @@
 </section>
 <section class="features-section-alt">
   <header class="section-header">
-    <h2 class="subheader">Find your friends</h2>
-    <h1>And Get Social</h1>
+    <h2 class="subheader">Upload all of your movie collections</h2>
+    <h1>With Our Passport</h1>
   </header>
   <div class="row">
     <div class="features-block">
       <img src="">
     </div>
     <div class="features-block">
-      <p>
+      <p class="summary">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
       <p>
@@ -140,9 +139,6 @@
   </div>
 </section>
 <section class="features-section">
-  <div class="request-invite">
-    <h1>Request an invite to get started</h1>
-  </div>
   <div class="sign-up-container">
     <a href="#" class="sign-up">
       <img src="img/png/stamps/Request_blue.png" alt="Sign up here">
