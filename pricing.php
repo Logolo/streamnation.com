67b08b085af7eecf1f5e40f308171ba40f030c11
Justin Falcone <stanulam59@gmail.com>
2013-02-27 21:33:23 -0500

    pricing page

--- pricing.php
+++ pricing.php
@@ -1,5 +1,89 @@
 <?php require 'header.php' ?>
 
-<h1>TBD: Pricing</h1>
+<section class="pricing-content">
+  <header class="section-header">
+    <h1>Join Now for Free</h1>
+    <p>
+      Try Stream Nation for 15 free days! Have all of your videos in one place, on any device, all the time. After you try Stream Nation, we have options for you to find the plan that will fit your needs.
+    </p>
+  </header>
+  <a href="#" class="pricing-signup-button">Request an Invite!</a>
+  <div class="row">
+    <div class="pricing-block">
+      <h1><span class="num">100</span> GB</h1>
+      <div class="space">
+        100 GB of storage
+        <span class="separator">is</span>
+        100 movies
+        <span class="separator">or</span>
+        400 episodes
+      </div>
+      <div class="price">
+        <p>$4.99<span>/month</span></p>
+        <p>$49.99<span>/year</span></p>
+      </div>
+    </div>
+    <div class="pricing-block">
+      <h1><span class="num">500</span> GB</h1>
+      <div class="space">
+        500 GB of storage
+        <span class="separator">is</span>
+        500 movies
+        <span class="separator">or</span>
+        800 episodes
+      </div>
+      <div class="price">
+        <p>$9.99<span>/month</span></p>
+        <p>$99.99<span>/year</span></p>
+      </div>
+    </div>
+    <div class="pricing-block">
+      <h1><span class="num">1</span> TB</h1>
+      <div class="space">
+        1 TB of storage
+        <span class="separator">is</span>
+        1000 movies
+        <span class="separator">or</span>
+        1600 episodes
+      </div>
+      <div class="price">
+        <p>$14.99<span>/month</span></p>
+        <p>$149.99<span>/year</span></p>
+      </div>
+    </div>
+    <div class="pricing-block-best">
+      <h1>Unlimited</h1>
+      <div class="space">
+        Unlimited storage
+        <span class="separator">is</span>
+        Unlimited movies
+        <span class="separator">and</span>
+        Unlimited episodes
+      </div>
+      <div class="price">
+        <p>$19.99<span>/month</span></p>
+        <p>$199.99<span>/year</span></p>
+      </div>
+    </div>
+  </div>
+  <div class="passport-pricing">
+    <div class="grid6">
+      <h2 class="subheader">Your passport to</h2>
+      <h1>Stream Nation</h1>
+      <p>You fill it up with your video library and drop it back at any Fedex in the envelope we provide you. We process and add all your videos to your account so it is ready to be watched on any of your devices.</p>
+      <ul>
+        <li>We will offer a 500GB Passport to all our Yearly Subscribers on 500GB, 1TB or Unlimited plans</li>
+        <li>Users will be able to order 500GB and 1TB Passports afterwards</li>
+        <li>500GB Passports will cost $49.99</li>
+        <li>1TB Passports will cost $99.99</li>
+      </ul>
+
+    </div>
+    <div class="grid6">
+      <img src="img/fedex_hdd.png">
+    </div>
+  </div>
+
+</section>
 
 <?php require 'footer.php' ?>
